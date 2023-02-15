dbf122d6178e345e8ce7e63dc86d5180fb53b3c6
dev_demir <dev_demir@outlook.com>
2023-02-15 21:54:55 +0300

    Initial Commit

---
+++ TodoMySelf/TodoMySelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMySelfApp.swift
+//  TodoMySelf
+//
+//  Created by Emrullah Demir on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoMySelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
